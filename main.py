f619c19257e8900bc9982eaadc5abd80d2e90344
Abrahan Fretes <abrahan.fretes@gmail.com>
2016-08-27 15:08:15 -0400

    División Frame Principal
    Signed-off-by: Abrahan Fretes <abrahan.fretes@gmail.com>

--- main.py
+++ main.py
@@ -18,6 +18,7 @@
 
 import wx
 from wx.lib.agw import aui
+from wx.lib.agw import customtreectrl as CT
 
 
 class MainFrame(wx.Frame):
@@ -36,24 +37,31 @@ class MainFrame(wx.Frame):
         self.SetTitle("FPUNA: Tavai")
         self.SetSize(wx.Size(800, 700))
         self.SetBackgroundColour("#F5D0A9")
-        self.SetMinSize((640, 480))
+        self.SetMinSize((660, 480))
 
     def v_content(self):
 
         # aui que manejará los paneles principales
-        self._mgr = aui.AuiManager(self)
+        self._mgr = aui.AuiManager(self, aui.AUI_MGR_ANIMATE_FRAMES)
         self.build_panels()
         pass
 
     def build_panels(self):
 
+        # tree panel
+        self._mgr.AddPane(TreePanel(self),
+                          aui.AuiPaneInfo().Name("tree_pane").
+                          Left().Layer(1).Position(1).CloseButton(False).
+                          MaximizeButton(True).MinimizeButton(True).
+                          Floatable(False))
+
         # Panel central
-        self._mgr.AddPane(MainPanel(self), aui.AuiPaneInfo().
+        self._mgr.AddPane(CentralPanel(self), aui.AuiPaneInfo().
                           Name("space_work_pane").CenterPane())
         self._mgr.Update()
 
 
-class MainPanel(wx.Panel):
+class CentralPanel(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
 
@@ -71,3 +79,21 @@ class MainPanel(wx.Panel):
         wel = wx.StaticText(self, -1, "Your Welcome to Tavai :)")
         wel.SetForegroundColour((255, 255, 255))
         self.sizer.Add(wel, 0, wx.ALIGN_CENTRE)
+
+
+class TreePanel(CT.CustomTreeCtrl):
+    def __init__(self, parent):
+        CT.CustomTreeCtrl.__init__(self, parent)
+
+        self.v_setting()
+
+        self.v_content()
+
+    def v_setting(self):
+        self.SetBackgroundColour("red")
+        self.SetSize(wx.Size(220, -1))
+        self.SetAGWWindowStyleFlag(CT.TR_HAS_BUTTONS | CT.TR_HIDE_ROOT)
+
+    def v_content(self):
+        self.root = self.AddRoot("TAVA TREE PROJECT", 0)
+        pass
